5b411d54134f6082b654071fbb03dde862306b8b
meidhika <meidhikanawa@gmail.com>
2025-08-17 10:56:06 +0700

    handle sigin

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,8 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../../components/navbar";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signInSchema } from "../../utils/zodSchema";
+import { useMutation } from "@tanstack/react-query";
+import { postSignIn } from "../../services/authService";
+import secureLocalStorage from "react-secure-storage";
+import { STORAGE_KEY } from "../../utils/const";
 
 export default function SignInPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(signInSchema),
+  });
+  const { isLoading, mutateAsync } = useMutation({
+    mutationFn: (data) => postSignIn(data),
+  });
+  const navigate = useNavigate();
+  const onSubmit = async (data) => {
+    try {
+      const response = await mutateAsync(data);
+      secureLocalStorage.setItem(STORAGE_KEY, response.data);
+      if (response.data.role === "manager") {
+        navigate("/manager");
+      } else {
+        navigate("/student");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="relative flex flex-col flex-1 p-[10px]">
       <div className="absolute w-[calc(100%-20px)] min-h-[calc(100vh-20px)] h-[calc(100%-20px)] bg-[#060A23] -z-10 rounded-[20px]">
@@ -27,7 +58,10 @@ export default function SignInPage() {
           </Link>
         </div>
       </nav>
-      <form className="flex flex-col w-[400px] h-fit rounded-[20px] border border-[#262A56] p-[30px] gap-[30px] bg-[#080A2A] m-auto">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col w-[400px] h-fit rounded-[20px] border border-[#262A56] p-[30px] gap-[30px] bg-[#080A2A] m-auto"
+      >
         <div>
           <h1 className="font-bold text-[26px] leading-[39px] text-white">
             Welcome Back!
@@ -38,7 +72,7 @@ export default function SignInPage() {
         <div className="flex items-center gap-3 w-full rounded-full border p-[14px_20px] transition-all duration-300 focus-within:border-[#8661EE] focus-within:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#070B24] border-[#24283E] shadow-[-10px_-6px_10px_0_#181A35_inset]">
           <img
             src="/assets/images/icons/sms-white.svg"
-            className="w-6 h-6 flex shrink-0"
+            className="flex w-6 h-6 shrink-0"
             alt="icon"
           />
           <input
@@ -47,13 +81,17 @@ export default function SignInPage() {
             id="email"
             className="appearance-none outline-none !bg-transparent w-full font-semibold text-white placeholder:font-normal placeholder:text-[#6B6C7F]"
             placeholder="Write your email address"
+            {...register("email")}
           />
         </div>
+        {errors.email?.message && (
+          <p className="-mt-5 text-xs text-red-500">{errors.email.message}</p>
+        )}
         <div>
           <div className="flex items-center gap-3 w-full rounded-full border p-[14px_20px] transition-all duration-300 focus-within:border-[#8661EE] focus-within:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#070B24] border-[#24283E] shadow-[-10px_-6px_10px_0_#181A35_inset]">
             <img
               src="/assets/images/icons/key-white.svg"
-              className="w-6 h-6 flex shrink-0"
+              className="flex w-6 h-6 shrink-0"
               alt="icon"
             />
             <input
@@ -62,8 +100,14 @@ export default function SignInPage() {
               id="password"
               className="appearance-none outline-none !bg-transparent w-full font-semibold text-white placeholder:font-normal placeholder:text-[#6B6C7F]"
               placeholder="Type your secure password"
+              {...register("password")}
             />
           </div>
+          {errors.password?.message && (
+            <p className="mt-3 text-xs text-red-500 ">
+              {errors.password.message}
+            </p>
+          )}
           <div className="flex justify-end mt-[10px]">
             <Link
               to="#"
@@ -75,6 +119,7 @@ export default function SignInPage() {
         </div>
         <hr className="border-[#262A56]" />
         <button
+          disabled={isLoading}
           type="submit"
           className="w-full rounded-full border p-[14px_20px] text-center font-semibold text-white bg-[#662FFF] border-[#8661EE] shadow-[-10px_-6px_10px_0_#7F33FF_inset]"
         >
