f7afd9f373033ef061e1ed9be3d2f23b61a42a6f
meidhika <meidhikanawa@gmail.com>
2025-08-20 19:21:58 +0700

    update layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,10 +1,16 @@
+import React from "react";
 import Sidebar from "./sidebar";
 import Header from "./header";
-import { Outlet, useMatch } from "react-router-dom";
+import Courses from "../pages/manager/home/courses";
+import Students from "../pages/manager/home/students";
+import { Outlet, useLoaderData, useMatch } from "react-router-dom";
 
 export default function LayoutDashboard({ isAdmin = true }) {
+  const session = useLoaderData();
+
   const isManagerPreviewPage = useMatch("/manager/courses/:id/preview");
   const isStudentPreviewPage = useMatch("/student/detail-course/:id");
+
   return (
     <>
       {isManagerPreviewPage !== null || isStudentPreviewPage !== null ? (
@@ -13,7 +19,7 @@ export default function LayoutDashboard({ isAdmin = true }) {
         <div className="flex min-h-screen">
           <Sidebar isAdmin={isAdmin} />
           <main className="flex flex-col flex-1 gap-[30px] p-[30px] ml-[290px]">
-            <Header />
+            <Header type={session?.role} />
             <Outlet />
           </main>
         </div>
