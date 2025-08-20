d335095f3ff40fc796b279ca5dc879a97315f486
meidhika <meidhikanawa@gmail.com>
2025-08-20 19:47:06 +0700

    fix routes

--- src/pages/SuccessCheckout/index.jsx
+++ src/pages/SuccessCheckout/index.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import Navbar from "../../components/navbar";
 import { Link } from "react-router-dom";
-export default function SuccessCheckout() {
+
+export default function SuccessCheckoutPage() {
   return (
     <div className="relative flex flex-col flex-1 p-[10px]">
       <div className="absolute w-[calc(100%-20px)] min-h-[calc(100vh-20px)] h-[calc(100%-20px)] bg-[#060A23] -z-10 rounded-[20px]">
@@ -28,9 +28,9 @@ export default function SuccessCheckout() {
       </nav>
       <h1 className="font-extrabold text-[46px] leading-[69px] text-white text-center m-auto">
         Success Checkout <br />
-        Please Login to Your Account
+        Please log in to continue
       </h1>
-      <Link to="/manager/sign-in">
+      <Link to="/managaer/sign-in">
         <div className="flex items-center justify-center gap-3 w-max mx-auto mt-5 rounded-full border p-[20px_50px] transition-all duration-300 hover:bg-[#662FFF] hover:border-[#8661EE] hover:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#662FFF] border-[#8661EE] shadow-[-10px_-6px_10px_0_#7F33FF_inset]">
           <span className="font-semibold text-white">Sign In Now</span>
         </div>
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,22 +2,33 @@ import { createBrowserRouter, redirect } from "react-router-dom";
 import ManagerHomePage from "../pages/manager/home";
 import SignInPage from "../pages/SignIn";
 import SignUpPage from "../pages/SignUp";
-import SuccessCheckout from "../pages/SuccessCheckout";
+import SuccessCheckoutPage from "../pages/SuccessCheckout";
 import LayoutDashboard from "../components/layout";
 import ManageCoursePage from "../pages/manager/courses";
-import ManageCreateCoursePage from "../pages/manager/create-course";
+import ManageCreateCoursePage from "../pages/manager/create-courses";
 import ManageCourseDetailPage from "../pages/manager/course-detail";
 import ManageContentCreatePage from "../pages/manager/course-content-create";
 import ManageCoursePreviewPage from "../pages/manager/course-preview";
 import ManageStudentsPage from "../pages/manager/students";
+import ManageStudentCreatePage from "../pages/manager/students-create";
 import StudentPage from "../pages/student/StudentOverview";
-import { MANAGER_SESSION, STORAGE_KEY } from "../utils/const";
 import secureLocalStorage from "react-secure-storage";
+import { MANAGER_SESSION, STORAGE_KEY, STUDENT_SESSION } from "../utils/const";
 import {
   getCategories,
   getCourseDetail,
   getCourses,
+  getDetailContent,
+  getStudentsCourse,
 } from "../services/courseService";
+import {
+  getCoursesStudents,
+  getDetailStudent,
+  getStudents,
+} from "../services/studentService";
+import StudentCourseList from "../pages/manager/student-course";
+import StudentForm from "../pages/manager/student-course/student-form";
+import { getOverviews } from "../services/overviewService";
 
 const router = createBrowserRouter([
   {
@@ -26,15 +37,33 @@ const router = createBrowserRouter([
   },
   {
     path: "/manager/sign-in",
+    loader: async () => {
+      const session = secureLocalStorage.getItem(STORAGE_KEY);
+
+      if (session && session.role === "manager") {
+        throw redirect("/manager");
+      }
+
+      return true;
+    },
     element: <SignInPage />,
   },
   {
     path: "/manager/sign-up",
+    loader: async () => {
+      const session = secureLocalStorage.getItem(STORAGE_KEY);
+
+      if (session && session.role === "manager") {
+        throw redirect("/manager");
+      }
+
+      return true;
+    },
     element: <SignUpPage />,
   },
   {
     path: "/success-checkout",
-    element: <SuccessCheckout />,
+    element: <SuccessCheckoutPage />,
   },
   {
     path: "/manager",
@@ -52,6 +81,11 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
+        loader: async () => {
+          const overviews = await getOverviews();
+
+          return overviews?.data;
+        },
         element: <ManagerHomePage />,
       },
       {
@@ -96,28 +130,111 @@ const router = createBrowserRouter([
         element: <ManageContentCreatePage />,
       },
       {
+        path: "/manager/courses/:id/edit/:contentId",
+        loader: async ({ params }) => {
+          const content = await getDetailContent(params.contentId);
+
+          return content?.data;
+        },
+        element: <ManageContentCreatePage />,
+      },
+      {
         path: "/manager/courses/:id/preview",
+        loader: async ({ params }) => {
+          const course = await getCourseDetail(params.id, true);
+
+          return course?.data;
+        },
         element: <ManageCoursePreviewPage />,
       },
       {
         path: "/manager/students",
+        loader: async () => {
+          const students = await getStudents();
+
+          return students?.data;
+        },
         element: <ManageStudentsPage />,
+      },
+      {
+        path: "/manager/students/create",
+        element: <ManageStudentCreatePage />,
+      },
+      {
+        path: "/manager/students/edit/:id",
+        loader: async ({ params }) => {
+          const student = await getDetailStudent(params.id);
+
+          return student?.data;
+        },
+        element: <ManageStudentCreatePage />,
+      },
+      {
+        path: "/manager/courses/students/:id",
+        loader: async ({ params }) => {
+          const course = await getStudentsCourse(params.id);
+
+          return course?.data;
+        },
+        element: <StudentCourseList />,
+      },
+      {
+        path: "/manager/courses/students/:id/add",
+        loader: async () => {
+          const students = await getStudents();
+
+          return students?.data;
+        },
+        element: <StudentForm />,
       },
     ],
   },
   {
     path: "/student",
+    id: STUDENT_SESSION,
+    loader: async () => {
+      const session = secureLocalStorage.getItem(STORAGE_KEY);
+
+      if (!session || session.role !== "student") {
+        throw redirect("/student/sign-in");
+      }
+
+      return session;
+    },
     element: <LayoutDashboard isAdmin={false} />,
     children: [
       {
         index: true,
+        loader: async () => {
+          const courses = await getCoursesStudents();
+
+          return courses?.data;
+        },
         element: <StudentPage />,
       },
       {
         path: "/student/detail-course/:id",
-        element: <ManageCoursePreviewPage />,
+        loader: async ({ params }) => {
+          const course = await getCourseDetail(params.id, true);
+
+          return course?.data;
+        },
+        element: <ManageCoursePreviewPage isAdmin={false} />,
       },
     ],
+  },
+  {
+    path: "/student/sign-in",
+    loader: async () => {
+      const session = secureLocalStorage.getItem(STORAGE_KEY);
+
+      if (session && session.role === "student") {
+        throw redirect("/student");
+      }
+
+      return true;
+    },
+    element: <SignInPage type="student" />,
   },
 ]);
 
