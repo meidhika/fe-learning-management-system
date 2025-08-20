25c52ec026d04195febd779819cf9840384bb444
meidhika <meidhikanawa@gmail.com>
2025-08-20 19:37:23 +0700

    course preview

--- src/pages/manager/course-preview/content-text.jsx
+++ src/pages/manager/course-preview/content-text.jsx
@@ -1,31 +1,22 @@
-import React from "react";
+import Proptypes from "prop-types";
 
-export default function ContentText() {
+export default function ContentText({ content, handleNext }) {
   return (
     <>
       <div className="flex flex-col gap-5 max-w-[800px] pb-[160px]">
         <h1 className="font-bold text-[32px] leading-[48px]">
-          Pengenalan UI UX
+          {content?.title}
         </h1>
-        <article id="Content-wrapper">
-          <p>
-            Dalam artikel ini, kita akan membahas tentang Laravel, mengapa
-            framework ini dibuat, alasan mengapa belajar Laravel, keunggulan
-            utama Laravel, persiapan yang diperlukan, langkah-langkah belajar
-            Laravel, dan bagaimana menemukan mentor serta proyek sampingan untuk
-            meningkatkan keterampilan Anda.
-          </p>
-          <h3>Dasar Design Principle</h3>
-          <p>
-            Laravel adalah framework open-source untuk PHP yang dibuat oleh
-            Taylor Otwell. Framework ini dirancang untuk pengembang web yang
-            ingin bekerja dengan cara yang elegan dan menyenangkan.
-          </p>
-        </article>
+        <article
+          id="Content-wrapper"
+          className="prose"
+          dangerouslySetInnerHTML={{ __html: content?.text }}
+        />
       </div>
       <div className="fixed bottom-0 w-[calc(100%-400px)] h-[151px] flex items-end justify-end pb-5 bg-[linear-gradient(0deg,#FFFFFF_49.67%,rgba(255,255,255,0)_84.11%)]">
         <button
           type="button"
+          onClick={() => handleNext(content)}
           className="w-fit rounded-full p-[14px_20px] font-semibold text-[#FFFFFF] bg-[#662FFF] text-nowrap"
         >
           Mark as Completed
@@ -34,3 +25,8 @@ export default function ContentText() {
     </>
   );
 }
+
+ContentText.propTypes = {
+  content: Proptypes.object,
+  handleNext: Proptypes.func,
+};
--- src/pages/manager/course-preview/content-video.jsx
+++ src/pages/manager/course-preview/content-video.jsx
@@ -1,25 +1,26 @@
-import React from "react";
+import Proptypes from "prop-types";
 
-export default function ContentVideo() {
+export default function ContentVideo({ content, handleNext }) {
   return (
     <>
       <div className="flex shrink-0 h-[calc(100vh-110px-104px)] rounded-[20px] overflow-hidden">
         <iframe
           className="w-full aspect-video"
-          src="https://www.youtube.com/embed/tYyPdH32faE?si=heerM3KnIDqdfrZJ"
+          src={`https://www.youtube.com/embed/${content?.youtubeId}?si=heerM3KnIDqdfrZJ`}
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
+          allowfullscreen
         ></iframe>
       </div>
       <div className="flex items-center justify-between gap-5">
         <h1 className="font-bold text-[32px] leading-[48px]">
-          Install Figma Plugins
+          {content?.title}
         </h1>
         <button
           type="button"
+          onClick={() => handleNext(content)}
           className="w-fit rounded-full p-[14px_20px] font-semibold text-[#FFFFFF] bg-[#662FFF] text-nowrap"
         >
           Mark as Completed
@@ -28,3 +29,8 @@ export default function ContentVideo() {
     </>
   );
 }
+
+ContentVideo.propTypes = {
+  content: Proptypes.object,
+  handleNext: Proptypes.func,
+};
